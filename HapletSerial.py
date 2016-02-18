f257d66499b5f6e60450ac690c9812c5f200ae24
Colin Gallacher <crgallac@cim.mcgill.ca>
2016-02-18 13:17:35 -0500

    initial commit

---
+++ HapletSerial.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import serial, struct
+import binascii
+import time
+
+print "Serial Test"
+
+foo= struct.Struct('<I')
+print foo.size
+
+
+
+ 
+ser = serial.Serial(
+        port='COM21',
+        baudrate= 115200, 
+        bytesize=serial.EIGHTBITS,
+        timeout=0.0001
+        
+)
+
+
+i=0
+n=0
+t0=0.0
+t1=0.0
+tMax=0.0
+nMax=0
+tDiff=0.0
+tAv=0.0
+
+
+##while (ser.readline().strip() != "initialized"):
+##        print "uninitialized"  
+
+##print "INITIALIZED"
+        
+while n<10000:
+
+        output= foo.pack(n)
+        ser.write(output)
+        t0=time.clock()
+        ##print t0
+
+
+        if(ser.inWaiting()):
+                incoming_data= foo.unpack(ser.read(foo.size))[0]; 
+                print incoming_data
+                print n-incoming_data
+                t1=time.clock()
+                tDiff=t1-t0
+                print '{0:.10f}'.format(tDiff)
+                if(tDiff)>tMax and n>1:
+                        tMax= t1-t0
+                        nMax= n
+                       
+                print ' '
+                tAv=tAv+tDiff
+                       
+        i=i+1
+
+        n=n+1
+
+
+
+
+
+ser.close()
+
+print tMax
+print nMax
+print tAv/n
+
